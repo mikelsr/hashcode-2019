0ecd92e4d9e3f04829f655598a7a0311f37f295f
mikelsr <mikelsr@pm.me>
2019-03-04 13:53:27 +0100

    Add external library to solve TSP, TODO:implement

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 from itertools import chain
 from sys import argv
 
+from tsp_solver.greedy import solve_tsp
+
 ENCODING = "ascii"
 INPUT_FILES = [
     "data/a_example.txt",
@@ -14,6 +16,7 @@ INPUT_FILES = [
 LIM = 1e7
 
 
+"""
 class Node:
     def __init__(self, slide):
         self.slide = slide
@@ -21,6 +24,7 @@ class Node:
 
     def __str__(self):
         return "{} {}".format(self.slide, ' '.join(["\n\tslide: {}\tcost: {}".format(x, y) for x, y in self.paths.items()]))
+"""
 
 
 # Pic only used for vertical photos
@@ -153,6 +157,7 @@ def pair_pics(pics):
     return slides
 
 
+"""
 def create_graph(slides):
     sorted_slides = sorted(list(slides), key=lambda x: len(x.tags))
     nodes = []
@@ -174,6 +179,29 @@ def create_graph(slides):
             n.paths[c] = cost
         nodes.append(n)
     return nodes
+"""
+
+
+def create_graph(slides):
+    ordered_slides = list(slides)
+    matrix = []
+    for s1 in ordered_slides:
+        row = []
+        for s2 in ordered_slides:
+            # cost between the same node = 0
+            if s1 is s2:
+                row.append(0)
+                continue
+            s = score(s1, s2)
+            row.append(1/s if s != 0 else 1.0)  # default cost is 1 (max)
+        matrix.append(row)
+    return matrix, ordered_slides
+
+
+def print_matrix(matrix, ordered_slides):
+    print("\t{}".format('\t'.join(["({})".format(s) for s in ordered_slides])))
+    for i in range(len(ordered_slides)):
+        print("({})\t{}".format(ordered_slides[i], '\t'.join(str(c) for c in matrix[i])))
 
 
 """
@@ -195,16 +223,18 @@ def main(n=0, out_dir="out"):
         ))
     # print("slides combined")
 
+    matrix, ordered = create_graph(slides)
+    order = solve_tsp(matrix)
+    final = [ordered[i] for i in order]
+
+
     # write output to file
-    nodes = create_graph(slides)
-    for n in nodes:
-        print(n)
-#    out = parse_output(slides)
-#    output_to_file(out, "{}/{}.out".format(out_dir, n))
+    out = parse_output(final)
+    output_to_file(out, "{}/{}.out".format(out_dir, n))
 
 
 if __name__ == "__main__":
-    n = 0
+    n = 3
     out_dir="out"
     if len(argv) > 1:
         n = int(argv[1])
